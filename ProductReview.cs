62114b6825a349f7c544613854a3b2eb3f2ca0f4
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-02-06 21:18:32 +0530

    Added files to remote

---
+++ ProductReview.cs
@@ -0,0 +1,12 @@
+namespace ProductReviewManagement
+{
+    public class ProductReview
+    {
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
+
+    }
+}
